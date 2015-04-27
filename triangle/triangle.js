b8905a0c3ee1c5e782b606ee0d46bb3cf92c66da
bndf <774199265@qq.com>
2015-04-27 13:38:35 +0800

    update

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -2,9 +2,39 @@ var triangle = {
     isEquilateral : function(a,b,c) {
     	    if(a!=b) return false ;
     	    if(b!=c) return false ;
-    		return true ;
+    	    if(a<=0) return false;
+    	   return true ;
     },
-    isIsosceles : function(a,b,c) {
-    	    return true ;
-    }
+    isIsosceles : function(a,b,c) 
+     {   
+    	    if(a!=b&&a!=c&&b!=c) return false ;
+    	    if(a+b<=c||a+c<=b||c+b<=a) return false ;
+    	    if((a+b<=c||a+c<=b||c+b<=a)&&(a-b>=c||a-c>=b||c-b>=a)) return false ;
+    	    if(a<=0) return false;
+    	    if(b<=0) return false;
+    	    if(c<=0) return false;
+    	    return true;
+    	    
+    },
+    isScalene : function(a,b,c) 
+    {   
+   	    if(a+b<=c||a+c<=b||c+b<=a) return false ;
+   	    if(a<=0) return false;
+   	    if(b<=0) return false;
+   	    if(c<=0) return false;
+   	    return true;
+   	    
+   }
+    
 };
+var quadrilateral={
+		
+		isQuadri:function(a,b,c,d)
+		{
+			
+			if(a<=0) return false;
+    	    if(b<=0) return false;
+    	    if(c<=0) return false;
+    	    if(d<=0) return false;
+		}
+}
